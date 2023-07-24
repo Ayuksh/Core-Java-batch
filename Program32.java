f9b48b9b672942f91201bb9d7de236fbefb376fe
Ayush <ayuksh05@gmail.com>
2023-07-24 10:11:21 +0530

    6th commit

---
+++ Program32.java
@@ -0,0 +1,15 @@
+class Program32
+{
+	public static void main(String[] args)
+	{
+		System.out.println("MAIN START !!");
+		display();
+		System.out.println("MAIN END !!");	
+	}
+	
+	public static void display()
+	{
+		System.out.println("DISPLAY METHOD !!");
+		display();
+	}
+}
