52431cef47e4d627fbe29a15067162ae5c351666
Ayush <ayuksh05@gmail.com>
2023-08-08 08:43:59 +0530

    Array example

---
+++ JHA-JFRCJD-M3/src/array/ArrayCreation.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayCreation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER SIZE OF ARRAY ");
+        int n = sc.nextInt();
+        int[] arr = new int[ n ] ;
+
+        for (int i =0 ; i < n ; i++)
+        {
+            System.out.println("ENTER STRING VALUE ");
+            int s = sc.nextInt();
+                arr[i] = s ;
+        }
+
+        System.out.println("----------------------");
+        int sum =0 ;
+        for (int s : arr)
+            sum += s ;
+
+        System.out.println("SUM IS "+ sum);
+
+    }
+}
