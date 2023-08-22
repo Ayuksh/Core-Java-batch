cb2f0c2f063b6fa8ed7451ec1dab7e3449427893
Ayush <ayuksh05@gmail.com>
2023-08-22 12:26:40 +0530

    multi level inheritance

--- JHA-JFRCJD-M3/src/inhertanceProgram/MainApp1.java
+++ JHA-JFRCJD-M3/src/inhertanceProgram/MainApp1.java
@@ -2,12 +2,12 @@ package inhertanceProgram;
 
 public class MainApp1 {
     public static void main(String[] args) {
-        BMW b1 = new BMW();
+        BMW b1 = new BMW(); // subclass
         b1.horsePower();
         b1.topSpeed();
         b1.safety();
 
-        Car c1 = new Car();
+        Car c1 = new Car(); // super class
         c1.horsePower();
         c1.topSpeed();
     }
