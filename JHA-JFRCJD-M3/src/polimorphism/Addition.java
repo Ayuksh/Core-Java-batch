99839b7b604355b8767af084682ae443caf87d65
Ayush <ayuksh05@gmail.com>
2023-09-01 13:07:19 +0530

    polymorphism

---
+++ JHA-JFRCJD-M3/src/polimorphism/Addition.java
@@ -0,0 +1,23 @@
+package polimorphism;
+
+public class Addition {
+    void add(int a )
+    {
+        System.out.println(" A "+ a);
+    }
+
+    void add(int a , int b )
+    {
+        System.out.println("A + B :"+(a+b));
+    }
+
+    void add(int a , int b , int c)
+    {
+        System.out.println("A + B + C "+ (a+b+c));
+    }
+
+    void sub()
+    {
+
+    }
+}
